40a668635202e90e02879245884e67c11997b9da
Aayush-at-workspace <aayush.ver.workspace@gmail.com>
2025-05-09 15:38:24 +0530

    Delete 19-  Flash Card directory

--- 19-  Flash Card/Script/Home.js
+++
@@ -1,9 +0,0 @@
-function create(){
-    window.location.href="Create.html";
-}
-function revise(){
-    window.location.href="Revise.html";
-}
-function view(){
-    window.location.href="ViewSet.html";
-}

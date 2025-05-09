4773d7fe6be4f82bea5ae7459cf6bc1fddd7b901
Aayush-at-workspace <aayush.ver.workspace@gmail.com>
2025-05-09 15:43:19 +0530

    Add files via upload

---
+++ Script/Home.js
@@ -0,0 +1,9 @@
+function create(){
+    window.location.href="Create.html";
+}
+function revise(){
+    window.location.href="Revise.html";
+}
+function view(){
+    window.location.href="ViewSet.html";
+}
